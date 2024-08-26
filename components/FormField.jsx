2730591a6a24ddf7da135e689f4cc8df2f39b721
= <pslopez91@soy.sena.edu.co>
2024-08-26 18:09:30 -0500

    docs: cambio en los colores de letra y contenedores de registro

--- components/FormField.jsx
+++ components/FormField.jsx
@@ -9,12 +9,12 @@ const FormField = ({title, value, placeholder, handleChangeText, otherStyles, ..
   return (
     <View className={`space-y-2 ${otherStyles}`}>
 
-      <Text className="text-base text-gray-100 font-pmedium">{title}</Text>
+      <Text className="text-base text-black-100 font-pmedium">{title}</Text>
 
-      <View className="border-2 border-black-200 w full h-16 px-4 bg-black-100 rounded-2xl
+      <View className="border-2 border-white-200 w full h-16 px-4 bg-white-100 rounded-2xl
       focus:border-secondary items-center">
         <TextInput
-          className="flex-1 text-white font-psemibold text-base"
+          className="flex-1 text-black font-psemibold text-base"
           value={value}
           placeholder={placeholder}
           placeholderTextColor="#7B7B8B"
